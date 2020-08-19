843af50f7fa7bafc76d99b299e85988576c354c7
tamojit2000 <tamojitdas2000@gmail.com>
2020-08-19 13:29:47 +0530

    Assignment 3 updated

--- Assignment_3/CUSTOMDATA.java
+++ Assignment_3/CUSTOMDATA.java
@@ -7,20 +7,28 @@ class CUSTOMDATA{
     arr=new int[11];
     arr[0]=0;
     pointer=1;
-    //System.out.println("INITIALIZED");
+    System.out.println("INITIALIZED");
   }
 
   void insert(int x){
     arr[pointer]=x;
     pointer++;
     arr[0]++;
-    //System.out.println("INSERTED");
+    System.out.println("INSERTED "+x);
+  }
+
+  void remove(){
+    pointer--;
+    arr[0]--;
+    System.out.println("Last item removed");
   }
 
   void display(){
+    System.out.println("\n----DATA-START------");
     for(int i=1;i<=pointer-1;i++){
       System.out.println(arr[i]);
     }
+    System.out.println("----DATA-END-----\n");
   }
 
 }
--- Assignment_3/Q2.java
+++ Assignment_3/Q2.java
@@ -1,19 +1,44 @@
+import java.util.Scanner;
+
 class Q2{
   public static void main(String[] args) {
     CUSTOMDATA data=new CUSTOMDATA();
-    data.initialize();
-    data.display();
-    data.insert(5);
-    data.insert(7);
-    data.insert(8);
-    data.display();
+    Scanner sc=new Scanner(System.in);
+    int opt,value;
+    while(true){
+      Menu();
+      opt=sc.nextInt();
+      if(opt==1){
+        data.initialize();
+      }
+      else if(opt==2){
+        System.out.println("Enter value");
+        value=sc.nextInt();
+        data.insert(value);
+      }
+      else if(opt==3){
+        data.remove();
+      }
+      else if(opt==4){
+        data.display();
+      }
+      else if(opt==5){
+        break;
+      }
+      else{
+        System.out.println("Wrong opt");
+      }
+    }
 
-    data.initialize();
-    data.display();
-    data.insert(1);
-    data.insert(2);
-    data.insert(3);
-    data.display();
 
+  }
+  static void Menu(){
+    System.out.println(""
+    +"MENU:\n\n"
+    +"1.Initialize\n"
+    +"2.INSERTED\n"
+    +"3.Remove\n"
+    +"4.Display\n"
+    +"5.Quit\n\n");
   }
 }
